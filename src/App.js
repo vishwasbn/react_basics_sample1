7fb31dc7fcca3e0b332c869001ae886ee77d43cf
Vishwas <vishwasbn@suyati.com>
2022-09-12 23:17:01 +0530

    first get api integration

--- src/App.js
+++ src/App.js
@@ -3,9 +3,10 @@ import './App.css'
 import Banner from "./Components/Banner/Banner";
 import RowPost from "./Components/RowPost/RowPost";
 import axios from "axios";
+import {useState} from 'react';
 
 function App() {
-
+  const [data, setData] = useState([]);
   return (
     <div className="app">
       <NavBar />
@@ -14,10 +15,21 @@ function App() {
       <button onClick={() => {
         axios.get('https://jsonplaceholder.typicode.com/posts').then(
           (response) => {
-            console.log(response.data)
+            //console.log(response.data)
+            setData(response.data);
           }
         )
       }}>Click Me</button>
+      {
+        data.map((entry, index)=>{
+          return(
+            <div>
+              <span>{index+1}  <h4>{entry.title}</h4></span>
+              <h5>{entry.body}</h5>
+            </div>
+          );
+        })
+      }
     </div>
   );
 }
