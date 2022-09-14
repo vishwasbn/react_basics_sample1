8c75caf68b38e56dc1d822e700bfb2f91de97fec
Vishwas <vishwasbn@suyati.com>
2022-09-14 16:41:55 +0530

    added trailer youtube display functionality

--- src/Components/RowPost/RowPost.js
+++ src/Components/RowPost/RowPost.js
@@ -1,10 +1,19 @@
 import React, { useEffect, useState } from 'react'
 import axios from '../../Axios'
-import { image_url } from '../../Constants';
+import YouTube from 'react-youtube';
+import { api_key, image_url } from '../../Constants';
 import './RowPost.css'
 
 function RowPost(props) {
-
+  const opts = {
+    height: '390',
+    width: '100%',
+    playerVars: {
+      // https://developers.google.com/youtube/player_parameters
+      autoplay: 1,
+    },
+  };
+  const [videoId, setVideoId] = useState();
   const [movieList, setMovieList] = useState([]);
   useEffect(() => {
     axios.get(props.url)
@@ -13,6 +22,22 @@ function RowPost(props) {
         console.log(response.data)
       })
   }, [props.url])
+
+  const handleMovieTrailer = (movieId) => {
+
+    axios.get(`movie/${movieId}/videos?language=en-US&api_key=${api_key}`)
+      .then(response => {
+        console.log(response.data.results)
+        if (response.data.results.length !== 0) {
+          setVideoId(response.data.results[0].key)
+        }
+        else {
+          setVideoId(null);
+        }
+
+      })
+    // setVideoId('ebzy_VT1Tg0');
+  }
 
   return (
     <div>
@@ -25,8 +50,8 @@ function RowPost(props) {
               return (
                 // <img className='poster' alt={movie.original_name ? movie.original_name : movie.original_title}
                 //   src={image_url + movie.poster_path} key={index}/>
-                <img className={props.isSmall ? 'smallPoster' : 'poster'} alt={movie.original_name ? movie.original_name : movie.original_title}
-                  src={image_url + movie.backdrop_path} key={index}/>
+                <img onClick={() => handleMovieTrailer(movie.id)} className={props.isSmall ? 'smallPoster' : 'poster'} alt={movie.original_name ? movie.original_name : movie.original_title}
+                  src={image_url + movie.backdrop_path} key={index} />
               );
             })
           }
@@ -34,6 +59,7 @@ function RowPost(props) {
 
 
         </div>
+        {videoId && <YouTube videoId={videoId} opts={opts} />}
       </div>
     </div>
   )
